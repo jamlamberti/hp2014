fbc9534864927518e729edd3b0821c4cbac9e5da
James Lamberti <jal416@lehigh.edu>
2014-11-15 18:23:06 -0500

    Working spider for classes

--- fetch/class_spider.py
+++ fetch/class_spider.py
@@ -4,7 +4,7 @@ try:
 	import db_manager
 except:
 	import os, sys, inspect
-	current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+	currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 	parentdir = os.path.dirname(currentdir)
 	sys.path.insert(0, parentdir)
 	import db_manager
@@ -30,6 +30,7 @@ for study_program in study_program_matches:
 	study_program_page = urllib2.urlopen(url)
 	
 	if not study_program_page:
+		continue
 		raise Exception('Could not find study_program_page!')
 	
 	study_program_page_raw = study_program_page.read()
@@ -37,6 +38,7 @@ for study_program in study_program_matches:
 	course_match = re.findall(r'course_details\.xml\?courseid=(\d+)&#38;term=1154', study_program_page_raw, flags=re.S|re.I|re.M)
 	
 	if not course_match:
+		continue
 		raise Exception('Could not find course_match!')
 
 	for course in course_match:
@@ -45,6 +47,7 @@ for study_program in study_program_matches:
 		class_page = urllib2.urlopen(url)
 		
 		if not class_page:
+			continue
 			raise Exception('Could not find class_page!')
 		
 		class_page_raw = class_page.read()
@@ -54,13 +57,14 @@ for study_program in study_program_matches:
 		if not crn_match:
 			raise Exception('Could not find crn_match!')
 		if not crn_match:
+			continue
 			raise Exception('Could not find the CRN')
 		crn = crn_match.group(1).strip('\n').strip() 
 	
 		professor_name_match = re.search(r';stopDefAction\(event\);" target="_blank">\s*([^<>]*?)</a>', class_page_raw, flags=re.S|re.I|re.M)
 		if not professor_name_match:
+			continue
 			raise Exception('Could not find professor_name_match!')
-		
 		professor_name = professor_name_match.group(1).replace("\n" , "").replace("\t", "").replace("     ", " ")
 		print professor_name
 		db.execute_all("UPDATE courses set prof='%s' where crn=%s"%(professor_name, crn))
