3442c0d58711c4f5617f65aa58732fe16076305f
James Lamberti <jal416@lehigh.edu>
2014-11-15 05:08:24 -0500

    Spider and miner working

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,8 +4,8 @@ USE grades;
 DROP TABLE IF EXISTS `preparse`;
 CREATE TABLE IF NOT EXISTS `preparse`(
     `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `prof` VARCHAR(50) NOT NULL,
-    `school` VARCHAR(50) NOT NULL,
+    `prof` VARCHAR(75) NOT NULL,
+    `school` VARCHAR(100) NOT NULL,
     `helpfulness` INT(2) NOT NULL,
     `clarity` INT(2) NOT NULL,
     `easiness` INT(2) NOT NULL,
