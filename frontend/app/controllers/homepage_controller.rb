bc6eda166df2e0182d93ff25b60b1da5eeb692c1
Luka <lmilekic@gmail.com>
2014-11-16 00:32:16 -0500

    made things work better

--- frontend/app/controllers/homepage_controller.rb
+++ frontend/app/controllers/homepage_controller.rb
@@ -7,17 +7,21 @@ class HomepageController < ApplicationController
   	@hash_of_professors = {}
     @average_difficulty = 0;
 
-  	File.open("public/data.json", "r") do |file|
-  		i = 0
+    File.open("public/data.json", "r") do |file|
+      i = 0
       total = 0;
-  		file.each do |line|
+      file.each do |line|
         i+=1;
-  			tempHash = JSON.parse(line)
-  			id = tempHash.delete("id")
+        tempHash = JSON.parse(line)
+        id = tempHash.delete("id")
         total+= Integer(tempHash["difficulty"])
-  			@hash_of_professors[id] = tempHash
-  		end
-      @average_difficulty = total.to_f / i.to_f
-  	end
-  end
+        if(@hash_of_professors[id] != nil) ##It's already been put in there
+          @hash_of_professors[id] << tempHash
+        else
+         @hash_of_professors[id] = [tempHash]
+       end
+     end
+     @average_difficulty = total.to_f / i.to_f
+   end
+ end
 end
