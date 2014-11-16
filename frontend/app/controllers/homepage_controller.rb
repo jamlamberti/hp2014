0cc3e3b4f7ad74074fc93f3ead60ed56e95d3100
Luka <lmilekic@gmail.com>
2014-11-15 22:07:22 -0500

    Stuff should be working more?

--- frontend/app/controllers/homepage_controller.rb
+++ frontend/app/controllers/homepage_controller.rb
@@ -5,14 +5,19 @@ class HomepageController < ApplicationController
 
   def fetch_data
   	@hash_of_professors = {}
+    @average_difficulty = 0;
 
   	File.open("public/data.json", "r") do |file|
   		i = 0
+      total = 0;
   		file.each do |line|
+        i+=1;
   			tempHash = JSON.parse(line)
   			id = tempHash.delete("id")
+        total+= Integer(tempHash["difficulty"])
   			@hash_of_professors[id] = tempHash
   		end
+      @average_difficulty = total.to_f / i.to_f
   	end
   end
 end
